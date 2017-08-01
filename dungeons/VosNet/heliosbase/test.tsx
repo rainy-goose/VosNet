fb4edcb31de809cfe530ac0660abf929e99501b1
vosnet <connor.farnworth@gmail.com>
2017-08-01 09:47:42 -0500

    Helios Base, initial commit

---
+++ dungeons/VosNet/heliosbase/test.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="helioslocker" tilewidth="160" tileheight="24" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="160" height="24" source="../../../objects/vosnet/heliosbase/helioslocker.png"/>
+ </tile>
+</tileset>
